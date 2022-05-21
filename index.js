a014f7fe7ae1b0a38f246dba1318756b854be789
Dawn Olivier <dmarieolivier@gmail.com>
2022-05-21 11:11:52 -0400

    commit tree b27634ac01dd3231daeb8b52ba6befcc2733ab8f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) { /*function when posting to name and email*/
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        "name": name,
+        "email": email
+    })
+}
+return fetch("http://localhost:3000/users", configurationObject) /*response to and from server*/
+    .then(response => response.json())
+    .then(body => newContact(body.id))
+    .catch(response => document.body.append("Unauthorized Access"))
+}
+function newContact(newID){  /*handles post request and retrieves new ID*/
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(message)
+}
